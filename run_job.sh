43cfab62a4d83eea505a9b27d7f04fce1c2c15b6
Krish Agarwal <krishagarwal@live.com>
2025-10-16 02:15:46 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl apply -f yaml/run_true_monarch_slow.yaml
-kubectl apply -f yaml/run_true_monarch_fast.yaml
+kubectl apply -f yaml/run_monarch_slow.yaml
+kubectl apply -f yaml/run_monarch_fast.yaml
