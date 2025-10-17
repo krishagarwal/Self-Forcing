2024d9adb670a8c95e8080df51b3021949d59016
Krish Agarwal <krishagarwal@live.com>
2025-10-17 11:38:51 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl apply -f yaml/run_monarch_slow_0.85.yaml
-kubectl apply -f yaml/run_monarch_fast_0.85.yaml
+# kubectl apply -f yaml/run_monarch_fast_0.85.yaml
 kubectl apply -f yaml/run_monarch_slow_0.95.yaml
 kubectl apply -f yaml/run_monarch_fast_0.95.yaml
-# kubectl apply -f yaml/run_self_forcing.yaml
+kubectl apply -f yaml/run_self_forcing.yaml
