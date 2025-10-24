b64f14af78d4786a2812b7866c7d85f612871cf5
Krish Agarwal <krishagarwal@live.com>
2025-10-24 17:16:16 -0400

    update

--- wan/modules/causal_model.py
+++ wan/modules/causal_model.py
@@ -103,10 +103,7 @@ class MonarchAttnImplicitFn(torch.autograd.Function):
 
         _, (grad_Q, grad_K, grad_V, grad_aR, grad_cR) = torch.autograd.functional.vjp(O_from_QKV_aR_cR, (Q, K, V, aR_star, cR_star), v=grad_out, create_graph=False, strict=True)
 
-        def aR_cR_from_aR_cR(aRcR_in):
-            aR_in = aRcR_in[:b*a*f*k*j*h*d].view(b, a, f, k, j, h, d)
-            cR_in = aRcR_in[b*a*f*k*j*h*d:].view(b, h, a, f, k, j, 1)
-
+        def aR_cR_from_aR_cR(aR_in, cR_in):
             bR = torch.einsum("bafkjhd,bfklhd->bhafkjl", aR_in, K)
             z = bR.to(torch.float32) * (1.0 / (cR_in + eps)).clamp_max(1e4)
             z = z - z.amax(dim=-1, keepdim=True)
