b126ceaaffa1d19bd3f5ab126087b16c15952631
Krish Agarwal <krishagarwal@live.com>
2025-10-30 18:18:00 -0400

    update

--- trainer/ode.py
+++ trainer/ode.py
@@ -14,6 +14,8 @@ import time
 import os
 
 from utils.distributed import barrier, fsdp_wrap, fsdp_state_dict, launch_distributed_job
+from torchvision.io import write_video
+from pipeline import CausalInferencePipeline, CausalDiffusionInferencePipeline, BidirectionalInferencePipeline, BidirectionalDiffusionInferencePipeline
 
 
 class Trainer:
@@ -32,6 +34,7 @@ class Trainer:
         self.dtype = torch.bfloat16 if config.mixed_precision else torch.float32
         self.device = torch.cuda.current_device()
         self.is_main_process = global_rank == 0
+        self.causal = config.causal
         self.disable_wandb = config.disable_wandb
 
         # use a random seed for the training
@@ -43,12 +46,12 @@ class Trainer:
         set_seed(config.seed + global_rank)
 
         if self.is_main_process and not self.disable_wandb:
-            wandb.login(host=config.wandb_host, key=config.wandb_key)
+            # wandb.login(host=config.wandb_host, key=config.wandb_key)
             wandb.init(
                 config=OmegaConf.to_container(config, resolve=True),
                 name=config.config_name,
                 mode="online",
-                entity=config.wandb_entity,
+                # entity=config.wandb_entity,
                 project=config.wandb_project,
                 dir=config.wandb_save_dir
             )
@@ -104,8 +107,16 @@ class Trainer:
         # 7. (If resuming) Load the model and optimizer, lr_scheduler, ema's statedicts
         if getattr(config, "generator_ckpt", False):
             print(f"Loading pretrained generator from {config.generator_ckpt}")
-            state_dict = torch.load(config.generator_ckpt, map_location="cpu")[
-                'generator']
+            if config.generator_ckpt.endswith(".safetensors"):
+                from safetensors.torch import load_file
+                state_dict = load_file(config.generator_ckpt, device="cpu")
+                state_dict = {f"model.{k}" : v for k, v in state_dict.items()}
+            else:
+                state_dict = torch.load(config.generator_ckpt, map_location="cpu")
+            if "generator" in state_dict:
+                state_dict = state_dict["generator"]
+            elif "model" in state_dict:
+                state_dict = state_dict["model"]
             self.model.generator.load_state_dict(
                 state_dict, strict=True
             )
@@ -114,6 +125,11 @@ class Trainer:
 
         self.max_grad_norm = 10.0
         self.previous_time = None
+
+        inf_pipeline_type = (CausalInferencePipeline if self.causal else BidirectionalInferencePipeline) \
+            if hasattr(config, 'denoising_step_list') else (CausalDiffusionInferencePipeline if self.causal else BidirectionalDiffusionInferencePipeline)
+
+        self.val_pipeline = inf_pipeline_type(config, self.device, generator=self.model.generator, text_encoder=self.model.text_encoder, vae=self.model.vae)
 
     def save(self):
         print("Start gathering distributed model states...")
@@ -222,8 +238,47 @@ class Trainer:
                 logging.info("DistGarbageCollector: Running GC.")
             gc.collect()
 
+    def generate_video(self, pipeline, prompts, image=None):
+        batch_size = len(prompts)
+        sampled_noise = torch.randn(
+            [batch_size, self.model.num_training_frames, 16, 60, 104], device="cuda", dtype=self.dtype
+        )
+        video, _ = pipeline.inference(
+            noise=sampled_noise,
+            text_prompts=prompts,
+            return_latents=True
+        )
+        current_video = video.permute(0, 1, 3, 4, 2).cpu().numpy() * 255.0
+        return current_video
+
     def train(self):
-        while True:
+        while self.step <= 1000:
+            if self.step % 100 == 0:
+                with torch.no_grad():
+                    prompts = [
+                        "In the video, two people are working at a wooden desk, using an iMac computer. One person, wearing a white knit sweater, is using the apple wireless mouse with their right hand, while their left hand rests on the sleek white keyboard. Their movements are smooth yet intentional, suggesting they are focused on a task on the computer screen. The monitor displays a well-organized array of files and folders, hinting at a task that involves detailed organization or detailed data navigation. The second person, only subtly visible, sits closely by and appears to observe or assist, creating a collaborative atmosphere. Their presence adds a quiet dynamic to the scene, as if they are ready to provide input or guidance. Sticky notes with handwritten notes are attached to the monitor’s stand, adding a touch of personal organization amidst the digital workspace. The focus on the keyboard and mouse emphasizes a streamlined workflow, indicative of a productive work environment. The overall ambiance is calm and focuses on teamwork, technology, and efficient workspace management.",
+                        "A determined climber is scaling a massive rock face, showcasing exceptional strength and skill. The person, clad in a teal shirt and dark pants, climbs with precision, their movements measured and deliberate. They are secured by climbing gear, which includes ropes and a harness, emphasizing their commitment to safety. The rugged texture of the sandy-colored rock provides an imposing backdrop, adding drama and scale to the climb. In the distance, other large rock formations and sparse vegetation can be seen under a bright, overcast sky, contributing to the natural and adventurous atmosphere. The scene captures a moment of focus and challenge, highlighting the climber's tenacity and the breathtaking environment.",
+                        "In the video, a lone musician stands gracefully in front of a grand cathedral, playing an accordion while surrounded by the lively water display of a central fountain. Dressed in a casual ensemble, he wears a light-colored shirt, dark pants, and a flat cap that gives him a vintage charm. His posture is relaxed, yet engaged, as he sways gently in rhythm with the music, casting soft shadows on the cobblestone steps beneath him. The backdrop features the cathedral's towering twin spires, with intricate stonework that casts a rich, historical aura around the scene. Sunlight bathes the entire setting, enhancing the golden hues of the cathedral facade and creating a halo-like effect around the musician. The fountain's water jets splash playfully, catching glimmers of light and adding a dynamic element to the tranquil atmosphere. The scene captures a harmonious blend of architectural majesty and human creativity, framed by the clear, azure sky that extends infinitely above. It's a vivid depiction of solitude and artistry, set against a timeless urban landscape.",
+                        "In the video, a fluffy dog with brown patches is intently engaged with a bright red toy shaped like a fire hydrant, which has a yellow and orange rope attached. The dog's body is relaxed as it lies on a plain white background, concentrating on nudging and playfully biting the toy. Its ears perk up slightly with curiosity, and its eyes are fixated on the toy, suggesting a scene of focused playfulness. The neutral tones of the dog's fur contrast starkly against the vivid red of the toy, creating a visually striking moment.",
+                    ]
+                    validation = self.generate_video(
+                        self.val_pipeline,
+                        prompts=prompts,
+                    )
+                    if self.is_main_process and not self.disable_wandb:
+                        filenames = []
+                        for i, _ in enumerate(prompts):
+                            filename = f"/workspace/temp_{self.step}_{i}.mp4"
+                            write_video(filename, validation[i], fps=16)
+                            filenames.append(filename)
+                        logs = {
+                            f"validation_videos": [
+                                wandb.Video(filename, caption=prompt)
+                                for filename, prompt in zip(filenames, prompts)
+                            ]
+                        }
+                        wandb.log(logs, step=self.step)
+
             self.train_one_step()
             if (not self.config.no_save) and self.step % self.config.log_iters == 0:
                 self.save()
