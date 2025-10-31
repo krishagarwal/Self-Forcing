3e426034e306640a6991fdf1b001b0ad843e56b5
Krish Agarwal <krishagarwal@live.com>
2025-10-31 16:54:15 -0400

    updare

--- run_job.sh
+++ run_job.sh
@@ -4,6 +4,6 @@
 # kubectl apply -f yaml/run_monarch_slow_0.95.yaml
 # kubectl apply -f yaml/run_monarch_fast_0.95.yaml
 # kubectl apply -f yaml/run_self_forcing.yaml
-kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.85.yaml
-# kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.95.yaml
-kubectl apply -f yaml/run_wan_finetune_regular.yaml
+# kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.85.yaml
+kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.95.yaml
+# kubectl apply -f yaml/run_wan_finetune_regular.yaml
