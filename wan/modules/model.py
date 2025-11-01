5639358543e7dbe749d1e82510071b961fa0e867
Krish Agarwal <krishagarwal@live.com>
2025-11-01 16:59:33 -0400

    update

--- wan/modules/model.py
+++ wan/modules/model.py
@@ -13,46 +13,6 @@ from .attention import flash_attention
 __all__ = ['WanModel']
 
 class MonarchAttnImplicitFn(torch.autograd.Function):
-    # @staticmethod
-    # def forward(ctx, Q, K, V, sm_scale, num_iters, eps):
-    #     b, a, i, j, h, _ = Q.shape
-    #     block_b1, block_b2 = i, j
-    #     f = K.size(-5)
-
-    #     sm_scale_sqrt = sm_scale ** 0.5
-    #     Q = Q * sm_scale_sqrt
-    #     K = K * sm_scale_sqrt
-
-    #     L = torch.eye(block_b1, device=Q.device, dtype=Q.dtype).view(1, 1, 1, 1, 1, block_b1, block_b1).expand(b, h, a, f, block_b2, block_b1, block_b1) # (b, h, a, f, j, k, i)
-
-    #     with torch.no_grad():
-    #         for _ in range(num_iters):
-    #             aR = torch.einsum("bhafjki,baijhd->bafkjhd", L, Q)
-    #             bR = torch.einsum("bafkjhd,bfklhd->bhafkjl", aR, K)
-    #             # cR = torch.einsum("bhjki->bhkj", L_star).unsqueeze(-1)
-    #             # R = torch.softmax(bR / (cR + eps), dim=-1)
-    #             cR = L.sum(dim=-1, dtype=torch.float32).unsqueeze(-1).clamp_min(eps).transpose(-2, -3) # (b, h, a, f, k, j, 1)
-    #             z = bR.to(torch.float32) * (1.0 / (cR + eps)).clamp_max(1e4)
-    #             z = z - z.amax(dim=-1, keepdim=True)
-    #             R = torch.softmax(z, dim=-1).to(Q.dtype)
-
-    #             aL = torch.einsum("bhafkjl,bfklhd->bafjkhd", R, K)
-    #             bL = torch.einsum("bafjkhd,baijhd->bhafjki", aL, Q)
-    #             # cL = torch.einsum("bhkjl->bhjk", torch.xlogy(R, R)).unsqueeze(-1)
-    #             logz = torch.logsumexp(z, dim=-1, keepdim=True) # (b, h, a, f, k, j, 1)
-    #             cL = (R * (z - logz)).sum(dim=-1, keepdim=True).transpose(-2, -3) # (b, h, a, f, j, k, 1)
-    #             L = rearrange(bL - cL, "b h a f j k i -> b h a j i (f k)")
-    #             L = torch.softmax(L, dim=-1).to(Q.dtype)
-    #             L = rearrange(L, "b h a j i (f k) -> b h a f j k i", f=f, k=block_b1)
-
-    #     ctx.save_for_backward(L, R, Q, K, V)
-    #     ctx.eps = eps
-    #     ctx.sm_scale_sqrt = sm_scale_sqrt
-
-    #     out = torch.einsum("bhafkjl,bfklhd->bafjkhd", R, V)
-    #     out = torch.einsum("bhafjki,bafjkhd->baijhd", L, out)
-    #     return out
-
     def forward(ctx, Q, K, V, sm_scale, num_iters, eps):
         b, a, i, j, h, _ = Q.shape
         block_b1, block_b2 = i, j
