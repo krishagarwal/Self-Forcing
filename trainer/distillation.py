e465ccf268af3ba73c9f8d970964e78262bb8d69
Krish Agarwal <krishagarwal@live.com>
2025-11-01 16:43:28 -0400

    update

--- trainer/distillation.py
+++ trainer/distillation.py
@@ -1,5 +1,8 @@
 import gc
+import glob
 import logging
+import pickle
+import numpy as np
 
 from utils.dataset import ShardingLMDBDataset, cycle
 from utils.dataset import TextDataset
@@ -16,7 +19,7 @@ import wandb
 import time
 import os
 from torchvision.io import write_video
-from pipeline import CausalInferencePipeline
+from pipeline import CausalInferencePipeline, CausalDiffusionInferencePipeline, BidirectionalInferencePipeline, BidirectionalDiffusionInferencePipeline
 
 
 class Trainer:
@@ -29,12 +32,12 @@ class Trainer:
         torch.backends.cudnn.allow_tf32 = True
 
         launch_distributed_job()
-        global_rank = dist.get_rank()
+        self.global_rank = dist.get_rank()
         self.world_size = dist.get_world_size()
 
         self.dtype = torch.bfloat16 if config.mixed_precision else torch.float32
         self.device = torch.cuda.current_device()
-        self.is_main_process = global_rank == 0
+        self.is_main_process = self.global_rank == 0
         self.causal = config.causal
         self.disable_wandb = config.disable_wandb
 
@@ -44,7 +47,7 @@ class Trainer:
             dist.broadcast(random_seed, src=0)
             config.seed = random_seed.item()
 
-        set_seed(config.seed + global_rank)
+        set_seed(config.seed + self.global_rank)
 
         if self.is_main_process and not self.disable_wandb:
             # wandb.login(host=config.wandb_host, key=config.wandb_key)
@@ -158,11 +161,35 @@ class Trainer:
             print(f"Setting up EMA with weight {ema_weight}")
             self.generator_ema = EMA_FSDP(self.model.generator, decay=ema_weight)
 
+        checkpoint_folders = glob.glob(os.path.join(self.output_path, "checkpoint_model_*"))
+        if False:#len(checkpoint_folders) > 0:
+            checkpoint_folders = sorted(checkpoint_folders, key=lambda x: int(x.split("_")[-1]))
+            latest_folder = checkpoint_folders[-1]
+            checkpoint_path = os.path.join(latest_folder, "model.pt")
+            self.step = int(latest_folder.split("_")[-1])
+            print(f"Resuming from checkpoint {checkpoint_path} at step {self.step}")
+        else:
+            checkpoint_path = getattr(config, "generator_ckpt", None)
+            print("Starting training from scratch")
+
         ##############################################################################################################
         # 7. (If resuming) Load the model and optimizer, lr_scheduler, ema's statedicts
-        if getattr(config, "generator_ckpt", False):
-            print(f"Loading pretrained generator from {config.generator_ckpt}")
-            state_dict = torch.load(config.generator_ckpt, map_location="cpu")
+        if checkpoint_path is not None:
+            print(f"Loading pretrained generator from {checkpoint_path}")
+            if checkpoint_path.endswith(".safetensors"):
+                from safetensors.torch import load_file
+                state_dict = load_file(config.generator_ckpt, device="cpu")
+                state_dict = {f"model.{k}" : v for k, v in state_dict.items()}
+            else:
+                state_dict = torch.load(checkpoint_path, map_location="cpu")
+            if "generator_ema" in state_dict and self.generator_ema is not None:
+                self.generator_ema.load_state_dict(
+                    state_dict["generator_ema"], strict=True
+                )
+            if "critic" in state_dict:
+                self.model.fake_score.load_state_dict(
+                    state_dict["critic"], strict=True
+                )
             if "generator" in state_dict:
                 state_dict = state_dict["generator"]
             elif "model" in state_dict:
@@ -181,7 +208,82 @@ class Trainer:
         self.max_grad_norm_critic = getattr(config, "max_grad_norm_critic", 10.0)
         self.previous_time = None
 
-        self.val_pipeline = CausalInferencePipeline(config, self.device, generator=self.model.generator, text_encoder=self.model.text_encoder, vae=self.model.vae)
+        inf_pipeline_type = (CausalInferencePipeline if self.causal else BidirectionalInferencePipeline) \
+            if hasattr(config, 'denoising_step_list') else (CausalDiffusionInferencePipeline if self.causal else BidirectionalDiffusionInferencePipeline)
+
+        self.val_pipeline = inf_pipeline_type(config, self.device, generator=self.model.generator, text_encoder=self.model.text_encoder, vae=self.model.vae)
+        self.cpu_group = torch.distributed.new_group(list(range(self.world_size)), backend="gloo")
+        if config.validation_prompts_file is not None:
+            with open(config.validation_prompts_file, "r") as f:
+                all_prompts = [line.strip() for line in f.readlines()]
+            validate_first_n = config.validate_first_n if hasattr(config, 'validate_first_n') else len(all_prompts)
+            all_prompts = all_prompts[:validate_first_n]
+            self.val_prompts = all_prompts
+        else:
+            self.val_prompts = None
+
+    def send_object(self, obj, dst: int) -> None:
+        """Send the input object list to the destination rank."""
+        """NOTE: `dst` is the local rank of the destination rank."""
+
+        assert dst < self.world_size, f"Invalid dst rank ({dst})"
+
+        assert dst != self.global_rank, (
+            "Invalid destination rank. Destination rank is the same "
+            "as the current rank.")
+
+        # Serialize object to tensor and get the size as well
+        object_tensor = torch.frombuffer(pickle.dumps(obj), dtype=torch.uint8)
+
+        size_tensor = torch.tensor([object_tensor.numel()],
+                                   dtype=torch.long,
+                                   device="cpu")
+
+        # Send object size
+
+        torch.distributed.send(size_tensor,
+                               dst=dst,
+                               group=self.cpu_group)
+
+        # Send object
+        torch.distributed.send(object_tensor,
+                               dst=dst,
+                               group=self.cpu_group)
+
+        return None
+
+    def recv_object(self, src: int):
+        """Receive the input object list from the source rank."""
+        """NOTE: `src` is the local rank of the source rank."""
+
+        assert src < self.world_size, f"Invalid src rank ({src})"
+
+        assert src != self.global_rank, (
+            "Invalid source rank. Source rank is the same as the current rank.")
+
+        size_tensor = torch.empty(1, dtype=torch.long, device="cpu")
+
+        # Receive object size
+        rank_size = torch.distributed.recv(size_tensor,
+                                           src=src,
+                                           group=self.cpu_group)
+
+        # Tensor to receive serialized objects into.
+        object_tensor = torch.empty(  # type: ignore[call-overload]
+            size_tensor.item(),  # type: ignore[arg-type]
+            dtype=torch.uint8,
+            device="cpu")
+
+        rank_object = torch.distributed.recv(object_tensor,
+                                             src=src,
+                                             group=self.cpu_group)
+
+        assert rank_object == rank_size, (
+            "Received object sender rank does not match the size sender rank.")
+
+        obj = pickle.loads(object_tensor.numpy().tobytes())
+
+        return obj
 
     def save(self):
         print("Start gathering distributed model states...")
@@ -317,31 +419,42 @@ class Trainer:
         start_step = self.step
 
         while self.step <= 800:
-            if self.step % 100 == 0:
+            if self.step % 100 == 0 and not (self.disable_wandb or self.val_prompts is None):
                 with torch.no_grad():
-                    prompts = [
-                        "A movie trailer featuring the adventures of the 30 year old space man wearing a red wool knitted motorcycle helmet, blue sky, salt desert, cinematic style, shot on 35mm film, vivid colors.",
-                        "The camera rotates around a large stack of vintage televisions all showing different programs — 1950s sci-fi movies, horror movies, news, static, a 1970s sitcom, etc, set inside a large New York museum gallery.",
-                        "A white and orange tabby cat is seen happily darting through a dense garden, as if chasing something. Its eyes are wide and happy as it jogs forward, scanning the branches, flowers, and leaves as it walks. The path is narrow as it makes its way between all the plants. the scene is captured from a ground-level angle, following the cat closely, giving a low and intimate perspective. The image is cinematic with warm tones and a grainy texture. The scattered daylight between the leaves and plants above creates a warm contrast, accentuating the cat’s orange fur. The shot is clear and sharp, with a shallow depth of field.",
-                        "A stylish woman walks down a Tokyo street filled with warm glowing neon and animated city signage. She wears a black leather jacket, a long red dress, and black boots, and carries a black purse. She wears sunglasses and red lipstick. She walks confidently and casually. The street is damp and reflective, creating a mirror effect of the colorful lights. Many pedestrians walk about.",
-                    ]
+                    # prompts = [
+                    #     "In the video, two people are working at a wooden desk, using an iMac computer. One person, wearing a white knit sweater, is using the apple wireless mouse with their right hand, while their left hand rests on the sleek white keyboard. Their movements are smooth yet intentional, suggesting they are focused on a task on the computer screen. The monitor displays a well-organized array of files and folders, hinting at a task that involves detailed organization or detailed data navigation. The second person, only subtly visible, sits closely by and appears to observe or assist, creating a collaborative atmosphere. Their presence adds a quiet dynamic to the scene, as if they are ready to provide input or guidance. Sticky notes with handwritten notes are attached to the monitor’s stand, adding a touch of personal organization amidst the digital workspace. The focus on the keyboard and mouse emphasizes a streamlined workflow, indicative of a productive work environment. The overall ambiance is calm and focuses on teamwork, technology, and efficient workspace management.",
+                    #     "A determined climber is scaling a massive rock face, showcasing exceptional strength and skill. The person, clad in a teal shirt and dark pants, climbs with precision, their movements measured and deliberate. They are secured by climbing gear, which includes ropes and a harness, emphasizing their commitment to safety. The rugged texture of the sandy-colored rock provides an imposing backdrop, adding drama and scale to the climb. In the distance, other large rock formations and sparse vegetation can be seen under a bright, overcast sky, contributing to the natural and adventurous atmosphere. The scene captures a moment of focus and challenge, highlighting the climber's tenacity and the breathtaking environment.",
+                    #     "In the video, a lone musician stands gracefully in front of a grand cathedral, playing an accordion while surrounded by the lively water display of a central fountain. Dressed in a casual ensemble, he wears a light-colored shirt, dark pants, and a flat cap that gives him a vintage charm. His posture is relaxed, yet engaged, as he sways gently in rhythm with the music, casting soft shadows on the cobblestone steps beneath him. The backdrop features the cathedral's towering twin spires, with intricate stonework that casts a rich, historical aura around the scene. Sunlight bathes the entire setting, enhancing the golden hues of the cathedral facade and creating a halo-like effect around the musician. The fountain's water jets splash playfully, catching glimmers of light and adding a dynamic element to the tranquil atmosphere. The scene captures a harmonious blend of architectural majesty and human creativity, framed by the clear, azure sky that extends infinitely above. It's a vivid depiction of solitude and artistry, set against a timeless urban landscape.",
+                    #     "In the video, a fluffy dog with brown patches is intently engaged with a bright red toy shaped like a fire hydrant, which has a yellow and orange rope attached. The dog's body is relaxed as it lies on a plain white background, concentrating on nudging and playfully biting the toy. Its ears perk up slightly with curiosity, and its eyes are fixated on the toy, suggesting a scene of focused playfulness. The neutral tones of the dog's fur contrast starkly against the vivid red of the toy, creating a visually striking moment.",
+                    # ]
+                    bsz_per_gpu = len(self.val_prompts) // self.world_size
+                    local_prompts = self.val_prompts[self.global_rank * bsz_per_gpu: (self.global_rank + 1) * bsz_per_gpu]
                     validation = self.generate_video(
                         self.val_pipeline,
-                        prompts=prompts,
+                        prompts=local_prompts,
                     )
-                    if self.is_main_process and not self.disable_wandb:
-                        filenames = []
-                        for i, _ in enumerate(prompts):
+                    if self.is_main_process:
+                        all_prompts = local_prompts
+                        all_videos = [validation]
+
+                        for rank in range(1, self.world_size):
+                            recv_prompts = self.recv_object(src=rank)
+                            recv_videos = self.recv_object(src=rank)
+                            all_prompts.extend(recv_prompts)
+                            all_videos.append(recv_videos)
+
+                        all_videos = np.concatenate(all_videos, axis=0)
+
+                        log_videos = []
+                        for i, prompt in enumerate(all_prompts):
                             filename = f"/workspace/temp_{self.step}_{i}.mp4"
-                            write_video(filename, validation[i], fps=16)
-                            filenames.append(filename)
-                        logs = {
-                            f"validation_videos": [
-                                wandb.Video(filename, caption=prompt)
-                                for filename, prompt in zip(filenames, prompts)
-                            ]
-                        }
+                            write_video(filename, all_videos[i], fps=16)
+                            log_videos.append(wandb.Video(filename, caption=prompt))
+                        logs = { "validation_videos": log_videos }
                         wandb.log(logs, step=self.step)
+                    else:
+                        self.send_object(local_prompts, dst=0)
+                        self.send_object(validation, dst=0)
 
             TRAIN_GENERATOR = self.step % self.config.dfake_gen_update_ratio == 0
 
@@ -416,3 +529,6 @@ class Trainer:
                     if not self.disable_wandb:
                         wandb.log({"per iteration time": current_time - self.previous_time}, step=self.step)
                     self.previous_time = current_time
+        
+        torch.distributed.destroy_process_group(self.cpu_group)
+        self.cpu_group = None
