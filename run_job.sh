dcf5460fb0db1795625fcbc7d30aba02918d70d9
Krish Agarwal <krishagarwal@live.com>
2025-11-01 20:24:54 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -2,11 +2,11 @@
 # kubectl apply -f yaml/run_monarch_slow_0.85.yaml
 # kubectl apply -f yaml/run_monarch_fast_0.85.yaml
 # kubectl apply -f yaml/run_monarch_slow_0.95.yaml
-# kubectl apply -f yaml/run_monarch_fast_0.95.yaml
+kubectl apply -f yaml/run_monarch_fast_0.95.yaml
 # kubectl apply -f yaml/run_self_forcing.yaml
 # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.85.yaml
 # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.95.yaml
 # kubectl apply -f yaml/run_wan_finetune_regular.yaml
-kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame.yaml
-kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame_reduced_w.yaml
-kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame_reduced_h.yaml
+# kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame.yaml
+# kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame_reduced_w.yaml
+# kubectl apply -f yaml/run_wan_finetune_monarch_fast_cross_frame_reduced_h.yaml
