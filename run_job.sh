cca7cbcf0f6bc22236451af8c6c5093862a9915b
Krish Agarwal <krishagarwal@live.com>
2025-11-04 19:35:05 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -2,9 +2,9 @@
 # kubectl apply -f yaml/run_monarch_slow_0.85.yaml
 # kubectl apply -f yaml/run_monarch_fast_0.85.yaml
 # kubectl apply -f yaml/run_monarch_slow_0.95.yaml
-kubectl apply -f yaml/run_monarch_fast_0.95.yaml
+# kubectl apply -f yaml/run_monarch_fast_0.95.yaml
 kubectl apply -f yaml/run_monarch_fast_crossframe_reduced_h.yaml
-# kubectl apply -f yaml/run_monarch_fast_crossframe_reduced_w.yaml
+kubectl apply -f yaml/run_monarch_fast_crossframe_reduced_w.yaml
 # kubectl apply -f yaml/run_self_forcing.yaml
 # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.85.yaml
 # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.95.yaml
--- wan/modules/causal_model.py
+++ wan/modules/causal_model.py
@@ -719,13 +719,13 @@ class CausalWanSelfAttention(nn.Module):
             return (q_seq_len // w, w)
         if self.target_sparsity == 0.85:
             assert w % 2 == 0
-            if q_seq_len == (3 * h * w):
-                return (h, w // 2)
+            # if q_seq_len == (3 * h * w):
+            #     return (h, w // 2)
             return (q_seq_len // w, w // 2)
         else:
             assert h % 2 == 0
-            if q_seq_len == (3 * h * w):
-                return ((1, h // 2), w)
+            # if q_seq_len == (3 * h * w):
+            #     return ((1, h // 2), w)
             return ((q_seq_len // (h * w), q_seq_len // (2 * w)), w)
         # factors = [i for i in range(1, h + 1) if h % i == 0]
         # sparsities = [1 - (f*f*w + w*w*f)/(f*f*w*w) for f in factors]
