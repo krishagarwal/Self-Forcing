46ed54742fab31d888091fddea32e93d625aff27
Krish Agarwal <krishagarwal@live.com>
2025-11-13 18:20:46 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -12,7 +12,7 @@
 # # kubectl apply -f yaml/run_monarch_fast_crossframe_reduced_lowsparse_init.yaml
 # # kubectl apply -f yaml/run_monarch_fast_crossframe_lowsparse_init.yaml
 # kubectl apply -f yaml/run_self_forcing.yaml
-kubectl apply -f yaml/run_self_forcing_test.yaml
+# kubectl apply -f yaml/run_self_forcing_test.yaml
 # # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.85.yaml
 # # kubectl apply -f yaml/run_wan_finetune_monarch_fast_0.95.yaml
 # kubectl apply -f yaml/run_wan_finetune_regular.yaml
@@ -41,18 +41,23 @@ kubectl apply -f yaml/run_self_forcing_test.yaml
 # # kubectl apply -f yaml/run_topk_eval.yaml
 # kubectl apply -f yaml/run_monarch_fast_framewise_dense_init.yaml
 # kubectl apply -f yaml/run_monarch_fast_framewise_dense_init2.yaml
-# kubectl apply -f yaml/run_monarch_oracle0.yaml
+
+kubectl apply -f yaml/run_monarch_oracle0.yaml
 # kubectl apply -f yaml/run_monarch_oracle1.yaml
 # kubectl apply -f yaml/run_monarch_oracle2.yaml
 # kubectl apply -f yaml/run_monarch_oracle3.yaml
 # kubectl apply -f yaml/run_monarch_oracle4.yaml
 # kubectl apply -f yaml/run_monarch_oracle5.yaml
 # kubectl apply -f yaml/run_self_forcing_radial_attn_nohack.yaml
-# kubectl apply -f yaml/run_self_forcing_radial_attn.yaml
+kubectl apply -f yaml/run_self_forcing_radial_attn.yaml
 # kubectl apply -f yaml/run_self_forcing_svg_nohack.yaml
-# kubectl apply -f yaml/run_self_forcing_svg.yaml
-# kubectl apply -f yaml/run_self_forcing_topk.yaml
-# kubectl apply -f yaml2/run_self_forcing_ema.yaml
-# kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init3_ema.yaml
+kubectl apply -f yaml/run_self_forcing_svg.yaml
+kubectl apply -f yaml/run_self_forcing_topk.yaml
+kubectl apply -f yaml2/run_self_forcing_ema.yaml
+kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init_ema.yaml
+kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init2_ema.yaml
+kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init3_ema.yaml
 # kubectl apply -f yaml2/run_wan_finetune_regular_ema.yaml
 # kubectl apply -f yaml2/run_wan_finetune_monarch_fast_tied_frame_ema.yaml
+# kubectl apply -f yaml2/run_wan_finetune_monarch_fast_tied_frame2_ema.yaml
+# kubectl apply -f yaml2/run_wan_finetune_monarch_fast_tied_frame3_ema.yaml
