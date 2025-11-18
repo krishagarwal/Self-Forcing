11e4ae05fde99113d2d0a7bf28945f358bf54e09
Krish Agarwal <krishagarwal@live.com>
2025-11-18 02:38:39 -0500

    updat

--- run_job.sh
+++ run_job.sh
@@ -81,9 +81,9 @@
 
 ############## NEW RUNS ###############
 
-kubectl apply -f yaml/run_monarch_oracle0.yaml
-kubectl apply -f yaml/run_monarch_oracle1.yaml
-kubectl apply -f yaml/run_monarch_oracle2.yaml
+# kubectl apply -f yaml/run_monarch_oracle0.yaml
+# kubectl apply -f yaml/run_monarch_oracle1.yaml
+# kubectl apply -f yaml/run_monarch_oracle2.yaml
 kubectl apply -f yaml/run_self_forcing_topk.yaml
 
 # kubectl apply -f yaml/run_monarch_oracle0_exact.yaml
