7d8c24e4de584bc3a4ecd3a599c8b9f23a0acccb
Krish Agarwal <krishagarwal@live.com>
2025-11-19 01:22:33 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -82,7 +82,7 @@
 ############## NEW RUNS ###############
 
 # kubectl apply -f yaml/run_monarch_oracle0.yaml
-# kubectl apply -f yaml/run_monarch_oracle1.yaml
+kubectl apply -f yaml/run_monarch_oracle1.yaml
 # kubectl apply -f yaml/run_monarch_oracle2.yaml
 # kubectl apply -f yaml/run_self_forcing_topk.yaml
 
@@ -95,5 +95,9 @@
 # kubectl apply -f yaml2/run_self_forcing_ema.yaml
 # kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init_ema.yaml
 
-kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init2_ema.yaml
-kubectl apply -f yaml2/run_wan_finetune_monarch_fast_tied_frame2_ema.yaml
+# kubectl apply -f yaml2/run_monarch_fast_framewise_dense_init2_ema.yaml
+# kubectl apply -f yaml2/run_wan_finetune_monarch_fast_tied_frame2_ema.yaml
+
+kubectl apply -f yaml/run_wan_finetune_radial_attn.yaml
+kubectl apply -f yaml/run_wan_finetune_svg.yaml
+kubectl apply -f yaml/run_wan_finetune_topk.yaml
