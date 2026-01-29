d8bbf15ff47e08f02f20663650cb25041bea546b
Krish Agarwal <krishagarwal@live.com>
2026-01-28 23:04:22 -0500

    update

--- run_job.sh
+++ run_job.sh
@@ -159,8 +159,8 @@ kubectl apply -f yaml/run_self_forcing_inference_monarch_fast_framewise_reduce2x
 # kubectl apply -f yaml4/self_forcing_inference_svg.yaml
 # kubectl apply -f yaml4/self_forcing_inference_topk.yaml
 # kubectl apply -f yaml4/self_forcing_inference.yaml
-# kubectl apply -f yaml4/self_forcing_inference_monarch_fast_framewise.yaml
-# kubectl apply -f yaml4/self_forcing_inference_monarch_slow_framewise.yaml
+kubectl apply -f yaml4/self_forcing_inference_monarch_fast_framewise.yaml
+kubectl apply -f yaml4/self_forcing_inference_monarch_slow_framewise.yaml
 # kubectl apply -f yaml4/self_forcing_inference_radial_attn.yaml
-# kubectl apply -f yaml4/self_forcing_inference_monarch_fast_framewise_reduce2xh.yaml
-# kubectl apply -f yaml4/self_forcing_inference_monarch_slow_framewise_reduce2xh.yaml
+kubectl apply -f yaml4/self_forcing_inference_monarch_fast_framewise_reduce2xh.yaml
+kubectl apply -f yaml4/self_forcing_inference_monarch_slow_framewise_reduce2xh.yaml
